c91f1a20d5d5e4bdc771d9dce2d591873a9678f7
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-03-12 13:30:58 -0300

    design

--- src/index.js
+++ src/index.js
@@ -14,19 +14,39 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 
+function SimpleCounter(props){
+    return(
+        <div className="container">
+            <div className="one">0</div>
+            <div className="two">0</div>
+            <div className="three">0</div>
+            <div className="four">0</div>
+        </div>
+    )
+}
 
-class Home extends Component {
-
-    __constructor(){
-        super();
-        this.state={}
+/*class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
     }
+
+increment = () => {
+    this.state({count: this.state.count +1})
+}
+
     render (){
         return(
-            <h1> Proyecto React Vacio</h1>
+            <div className="container">
+                <button classname="minutes">+</button>
+                <button classname="seconds">-</button>
+                <h2>{this.state.count}</h2>
+            </div>
         )
     }
-}
+}*/
  
 
-ReactDOM.render(<Home />, document.querySelector("#root"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#root"));
